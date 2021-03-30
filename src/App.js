a8900c184a7a6c1b401ef50944fb329a6ebc70ae
cxq <a1057492951@gmail.com>
2021-03-31 11:37:42 +1300

    Automate running ESLint and prettify with every git commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
- class App extends Component {
- constructor() {
- super();
- this.state = {
- count: 0,
- }
- }
- makeIncrementer = amount => () =>
- this.setState(prevState => ({
- count: prevState.count + amount,
- }));
- increment = this.makeIncrementer(1);
- render() {
- return (
- <div>
- <p>Count: {this.state.count}</p>
- <button className="increment" onClick={this.increment}>Increment
-count</button>
- </div>
- )
- }
- }
- export default App;
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from "react"
+
+class App extends Component {
+  increment = this.makeIncrementer(1)
+
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+    }
+  }
+
+  makeIncrementer = (amount) => () =>
+    this.setState((prevState) => ({
+      count: prevState.count + amount,
+    }))
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button type="button" className="increment" onClick={this.increment}>
+          Increment count
+        </button>
+      </div>
+    )
+  }
+}
+export default App
